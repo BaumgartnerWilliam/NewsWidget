5877dbd8ad5f5733df3f3f56049ef3e21c2ba4ab
William Baumgartner <baumgartner.william@gmail.com>
2020-08-02 13:11:56 +0200

    feat: main-layout

--- src/App.js
+++ src/App.js
@@ -6,18 +6,40 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="widget">
+          <div className="widget--title capitalize">
+            <h3>news</h3>
+            <div>
+              <select>
+                <option value="" selected>
+                  select me
+                </option>
+                <option>test</option>
+              </select>
+            </div>
+          </div>
+          <ul className="widget--content">
+            <li className="widget--item mt-2">
+              <h3 className="capitalize medium-text">something happened yesterday with a great opportunity</h3>
+              <p className="mt-1">
+                <span className="small-text pr-2">29/08/2017</span>
+                <span className="bubble-text small-text">hacker news</span>
+              </p>
+            </li>
+            <hr />
+            <li className="widget--item mt-2">
+              <h3 className="capitalize medium-text">something happened yesterday</h3>
+              <p className="mt-1">
+                <span className="small-text pr-2">29/08/2017</span>
+                <span className="bubble-text small-text">hacker news</span>
+              </p>
+            </li>
+            <hr />
+          </ul>
+          <div className="widget--action mt-2">
+            <button className="btn capitalize p-1">show more</button>
+          </div>
+        </section>
       </header>
     </div>
   );
