aed1f8a9ce0a07cbe53030e1a78e30c51d620a64
William Baumgartner <baumgartner.william@gmail.com>
2020-08-02 23:11:01 +0200

    feat: add NewsWidget component

---
+++ src/components/NewsWidget/NewsWidget.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Widget from '../Widget/Widget';
+import Select from '../Select/Select';
+import ArticleList from '../ArticleList/ArticleList';
+
+const NewsWidget = ({
+  filterOptions,
+  articles,
+  onLoadMoreArticles,
+  onFilterChange
+}) => (
+  <Widget aria-label="news-widget">
+    <Widget.Header title={'News'}>
+      <Select
+        aria-label="news-filter"
+        options={filterOptions}
+        defaultValue=""
+        onChange={onFilterChange}
+      />
+    </Widget.Header>
+    <Widget.Body aria-label="news-articles">
+      <ArticleList articles={articles} />
+    </Widget.Body>
+    <Widget.Footer aria-label="news-footer">
+      <button
+        aria-label="load-more-news"
+        onClick={onLoadMoreArticles}
+        className="btn capitalize p-1">
+        show more
+      </button>
+    </Widget.Footer>
+  </Widget>
+);
+
+NewsWidget.propTypes = {
+  filterOptions: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string,
+      text: PropTypes.string
+    })
+  ),
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      publishedAt: PropTypes.string.isRequired,
+      source: PropTypes.shape({
+        id: PropTypes.string,
+        name: PropTypes.string.isRequired
+      }).isRequired
+    })
+  ),
+  onLoadMoreArticles: PropTypes.func,
+  onFilterChange: PropTypes.func
+};
+
+export default NewsWidget;
---
+++ src/components/NewsWidget/NewsWidget.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import NewsWidget from './NewsWidget';
+import { createArticle, createFilterOptions } from '../../mocks';
+
+describe('NewsWidget component', () => {
+  const NewsWidgetSelector = 'news-widget';
+  const NewsFilterSelector = 'news-filter';
+  const NewsArticlesSelector = 'news-articles';
+  const ShowMoreSelector = 'load-more-news';
+
+  it('render without crashing', () => {
+    const { container } = render(<NewsWidget />);
+
+    expect(container).toBeInTheDocument();
+  });
+
+  it('renders a widget intitled news', () => {
+    const { getByLabelText, getByText } = render(<NewsWidget />);
+
+    getByLabelText(NewsWidgetSelector);
+    getByText('News');
+  });
+
+  it('display a news widget with filters and articles', () => {
+    const { getByLabelText, getByText } = render(
+      <NewsWidget
+        filterOptions={createFilterOptions(5)}
+        articles={createArticle(3)}
+      />
+    );
+
+    getByLabelText(NewsArticlesSelector);
+    getByLabelText(NewsFilterSelector);
+  });
+
+  it('should call the onFilterChange callback when the user changes it', async () => {
+    const filters = createFilterOptions(5);
+    const onFilterChange = jest.fn();
+    const { getByLabelText, getByText } = render(
+      <NewsWidget
+        filterOptions={filters}
+        articles={createArticle(3)}
+        onFilterChange={onFilterChange}
+      />
+    );
+    expect(onFilterChange).not.toHaveBeenCalled();
+
+    await wait(() => {
+      fireEvent.change(getByLabelText(NewsFilterSelector), {
+        target: { value: filters[4].value }
+      });
+    });
+
+    expect(onFilterChange).toHaveBeenCalledWith(filters[4].value);
+  });
+
+  it('should call the onLoadMoreArticles callback when the user click the show more button', async () => {
+    const onLoadMoreArticles = jest.fn();
+
+    const { getByLabelText, getByText } = render(
+      <NewsWidget
+        filterOptions={createFilterOptions(4)}
+        articles={createArticle(3)}
+        onLoadMoreArticles={onLoadMoreArticles}
+      />
+    );
+
+    expect(onLoadMoreArticles).not.toHaveBeenCalled();
+
+    await wait(() => {
+      fireEvent.click(getByLabelText(ShowMoreSelector));
+    });
+
+    expect(onLoadMoreArticles).toHaveBeenCalled();
+  });
+});
