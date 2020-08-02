e0582cc22f1010a3f1e6ec3f3a476e56ecfa53d9
William Baumgartner <baumgartner.william@gmail.com>
2020-08-02 19:37:41 +0200

    feat(App): update to use Widget component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
+import Article from './components/Article/Article';
+import Select from './components/Select/Select';
+import Widget from './components/Widget/Widget';
 import './App.css';
+
+const mockOptions = [
+  { value: '', text: 'filter by something' },
+  { value: 'select', text: 'my value' }
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <section className="widget">
-          <div className="widget--title capitalize">
-            <h3>news</h3>
-            <div>
-              <select>
-                <option value="" selected>
-                  select me
-                </option>
-                <option>test</option>
-              </select>
-            </div>
-          </div>
-          <ul className="widget--content">
-            <li className="widget--item mt-2">
-              <h3 className="capitalize medium-text">something happened yesterday with a great opportunity</h3>
-              <p className="mt-1">
-                <span className="small-text pr-2">29/08/2017</span>
-                <span className="bubble-text small-text">hacker news</span>
-              </p>
-            </li>
-            <hr />
-            <li className="widget--item mt-2">
-              <h3 className="capitalize medium-text">something happened yesterday</h3>
-              <p className="mt-1">
-                <span className="small-text pr-2">29/08/2017</span>
-                <span className="bubble-text small-text">hacker news</span>
-              </p>
-            </li>
-            <hr />
-          </ul>
-          <div className="widget--action mt-2">
+        <Widget>
+          <Widget.Header title={'News'}>
+            <Select options={mockOptions} defaultValue="" />
+          </Widget.Header>
+          <Widget.Body>
+            <ul className="article-list">
+              <li className="article-list--item mt-2">
+                <Article
+                  title={'something happened'}
+                  date={new Date()}
+                  editor={'my name'}
+                />
+              </li>
+              <hr />
+              <li className="article-list--item mt-2">
+                <Article
+                  title={'something happened'}
+                  date={new Date()}
+                  editor={'my name'}
+                />
+              </li>
+              <hr />
+            </ul>
+          </Widget.Body>
+          <Widget.Footer>
             <button className="btn capitalize p-1">show more</button>
-          </div>
-        </section>
+          </Widget.Footer>
+        </Widget>
       </header>
     </div>
   );
