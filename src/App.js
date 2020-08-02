85d69bd6a851f2ab7fd42c05ec477aaf9bf46a11
William Baumgartner <baumgartner.william@gmail.com>
2020-08-02 20:50:06 +0200

    feat(App): add unit tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import Article from './components/Article/Article';
 import Select from './components/Select/Select';
 import Widget from './components/Widget/Widget';
@@ -10,36 +9,52 @@ const mockOptions = [
   { value: 'select', text: 'my value' }
 ];
 
+const createArticle = n =>
+  Array(n)
+    .fill(0)
+    .map((_, idx) => (
+      <div key={idx}>
+        <li className="article-list--item mt-2">
+          <Article
+            title={'something happened'}
+            date={new Date()}
+            editor={'my name'}
+          />
+        </li>
+        <hr />
+      </div>
+    ));
+
 function App() {
+  const [articles, setNumberOfArticles] = useState(5);
+
+  const addArticles = () => setNumberOfArticles(articles + 5);
+  const filterArticles = value =>
+    value ? setNumberOfArticles(2) : setNumberOfArticles(5);
+
   return (
     <div className="App">
       <header className="App-header">
         <Widget>
           <Widget.Header title={'News'}>
-            <Select options={mockOptions} defaultValue="" />
+            <Select
+              options={mockOptions}
+              defaultValue=""
+              onChange={filterArticles}
+            />
           </Widget.Header>
           <Widget.Body>
-            <ul className="article-list">
-              <li className="article-list--item mt-2">
-                <Article
-                  title={'something happened'}
-                  date={new Date()}
-                  editor={'my name'}
-                />
-              </li>
-              <hr />
-              <li className="article-list--item mt-2">
-                <Article
-                  title={'something happened'}
-                  date={new Date()}
-                  editor={'my name'}
-                />
-              </li>
-              <hr />
+            <ul aria-label="article-list" className="article-list">
+              {createArticle(articles)}
             </ul>
           </Widget.Body>
           <Widget.Footer>
-            <button className="btn capitalize p-1">show more</button>
+            <button
+              aria-label="show-more"
+              onClick={addArticles}
+              className="btn capitalize p-1">
+              show more
+            </button>
           </Widget.Footer>
         </Widget>
       </header>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,67 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait, fireEvent } from '@testing-library/react';
 import App from './App';
 
-xtest('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App component', () => {
+  const filterSelector = 'select';
+  const articleSelector = 'article';
+  const articleListSelector = 'article-list';
+  const showMoreSelector = 'show-more';
+
+  it('renders withough crashing', () => {
+    const { container } = render(<App />);
+
+    expect(container).toBeInTheDocument();
+  });
+
+  it('renders the news widget', () => {
+    const { getByLabelText } = render(<App />);
+
+    getByLabelText('widget');
+  });
+
+  it('loads 5 unfiltered articles', () => {
+    const { getByLabelText, getAllByLabelText } = render(<App />);
+
+    getByLabelText(articleListSelector);
+    expect(getByLabelText(filterSelector).value).toBe('');
+    expect(getAllByLabelText(articleSelector).length).toBe(5);
+  });
+
+  it('loads 5 additional articles upon cliking on show more', async () => {
+    const { getByLabelText, getAllByLabelText } = render(<App />);
+
+    expect(getAllByLabelText(articleSelector).length).toBe(5);
+
+    await wait(() => {
+      fireEvent.click(getByLabelText(showMoreSelector));
+    });
+    expect(getAllByLabelText(articleSelector).length).toBe(10);
+  });
+
+  it('filters the articles upon changing it', async () => {
+    const filterName = 'select';
+    const unfilterName = '';
+    const { getByLabelText, getAllByLabelText } = render(<App />);
+
+    expect(getAllByLabelText(articleSelector).length).toBe(5);
+
+    await wait(() => {
+      fireEvent.change(getByLabelText(filterSelector), {
+        target: { value: filterName }
+      });
+    });
+
+    expect(getByLabelText(filterSelector).value).toBe(filterName);
+    expect(getAllByLabelText(articleSelector).length).toBe(2);
+
+    await wait(() => {
+      fireEvent.change(getByLabelText(filterSelector), {
+        target: { value: unfilterName }
+      });
+    });
+
+    expect(getByLabelText(filterSelector).value).toBe(unfilterName);
+    expect(getAllByLabelText(articleSelector).length).toBe(5);
+  });
+5});
