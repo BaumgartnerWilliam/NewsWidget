9ef95978d4242f5f01544a345c6404ecad3f470d
William Baumgartner <baumgartner.william@gmail.com>
2020-08-04 10:56:33 +0200

    feat(Article): make the title clickable links

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -3,11 +3,17 @@ import './Article.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
-const Article = ({ title, date, editor }) => (
+const Article = ({ title, date, editor, url }) => (
   <article aria-label="article" className="article">
     <h3 aria-label="title" className="title capitalize medium-text">
-      {title}
+      <a
+        className="title-link"
+        aria-label="title-link"
+        href={url}
+        target="_blank"
+        rel="noreferrer noopener">
+        {title}
+      </a>
     </h3>
     <p className="mt-1">
       <span aria-label="date" className="small-text pr-2">
@@ -23,7 +29,8 @@ const Article = ({ title, date, editor }) => (
 Article.propTypes = {
   title: PropTypes.string.isRequired,
   date: PropTypes.instanceOf(Date),
-  editor: PropTypes.string.isRequired
+  editor: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 };
 
 export default Article;
--- src/components/Article/Article.test.js
+++ src/components/Article/Article.test.js
@@ -3,6 +3,12 @@ import { render } from '@testing-library/react';
 import Article from './Article';
 
 describe('Article component', () => {
+  const titleSelector = 'title';
+  const dateSelector = 'date';
+  const editorSelector = 'editor';
+  const articleSelector = 'article';
+  const urlSelector = 'title-link';
+
   it('should render without crashing', () => {
     const props = {
       title: 'my title',
@@ -10,22 +16,24 @@ describe('Article component', () => {
       editor: 'editor name'
     };
     const { container, getByLabelText } = render(<Article {...props} />);
-    getByLabelText('article');
+    getByLabelText(articleSelector);
     expect(container).toBeInTheDocument();
   });
 
-  it('should render the title, the date and the editor properly', () => {
+  it('should render the title, url, the date and the editor properly', () => {
     const props = {
       title: 'my title',
       date: new Date(),
-      editor: 'editor name'
+      editor: 'editor name',
+      url: 'http://domain/my-url'
     };
     const { getByLabelText } = render(<Article {...props} />);
 
-    expect(getByLabelText('title').textContent).toBe('my title');
-    expect(getByLabelText('date').textContent).toBe(
+    expect(getByLabelText(titleSelector).textContent).toBe('my title');
+    expect(getByLabelText(dateSelector).textContent).toBe(
       props.date.toLocaleDateString()
     );
-    expect(getByLabelText('editor').textContent).toBe('editor name');
+    expect(getByLabelText(editorSelector).textContent).toBe('editor name');
+    expect(getByLabelText(urlSelector).href).toBe(props.url);
   });
 });
